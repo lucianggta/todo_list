3e87e5dce9df8937b02d45a17541d1e18445e002
lucianggta <59769903+lucianggta@users.noreply.github.com>
2020-12-05 17:18:42 +0100

    Working implementation of Todo list application

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,14 @@
 <template>
   <div class="wrapper">
-    <h1>What should I do?</h1>
-    <div class="newtodo">
-      <input
-        type="text"
-        name="todo-text"
-        v-model.trim="newTodoText"
-        class="todo-text"
-        placeholder="New todo"
-      />
-      <datepicker v-on:update="dateUpdated"/>
-      <button class="todo-add-button" v-on:click="addTodo()">Add</button>
+    <div id="newtodo">
+      <input type="text" name="todo-text" v-model.trim="newTodoText"
+        id="todo-text" placeholder="New todo">
+      <button v-on:click="addTodo()">Add</button>
     </div>
-    
-    <ul v-if="todos.length">
-      <li class="todo">
-        <span class="todo-text list-header">Todo</span>
-        <span class="todo-date list-header">Due date</span>
-        <span class="todo-empty-button list-header"></span>
-      </li>
-
-      <li class="todo" v-for="todo in todos" :key="todo.id">
-        <todo :todo="todo" v-on:remove="removeTodo(todo)"/>
+    <ul class="todo" v-if="todos.length">
+      <li v-for="todo in todos" :key="todo.id">
+        <span>{{ todo.text }}</span>
+        <button v-on:click="removeTodo(todo)">Remove</button>
       </li>
     </ul>
     <p class="none" v-else>Add a new todo in the input above</p>
@@ -29,55 +16,47 @@
 </template>
 
 <script>
-import datepicker from './components/datepicker.vue'
-import todo from './components/todo.vue'
-
 export default {
-  name: "App",
-  components: {
-    datepicker,
-    todo
-  },
-  data() {
-    return {
-      newTodoText: "",
-      newTodoDate: "",
-      todos: [],
-    };
-  },
-  methods: {
-    addTodo() {
-      if (this.newTodoText) {
-
-        this.todos.push({
-          text: this.newTodoText,
-          date: this.newTodoDate,
-          id: Date.now(),
-          done: false
-        });
-
-        this.todos.sort((todoA, todoB) => -todoA.date.diff(todoB.date))
-
-        this.newTodoText = "";
-      }
+    name: 'App',
+    components: {
     },
-    
-    removeTodo (item) {
-      this.todos = this.todos.filter((_item) => _item !== item);
+    data () {
+        return {
+            newTodoText: "",
+            todos: []
+        }
     },
-
-    dateUpdated (date) {
-      this.newTodoDate = date.clone()
+    methods: {
+        addTodo () {
+            // check if this.newTodoText is set (prevent adding of empty items)
+            if (this.newTodoText) {
+                // this.todos.push() adds a new entry to the back of a list
+                // a todo item consists of the text and an id, and we will simply use the current time for the id
+                this.todos.push({
+                    text: this.newTodoText,
+                    id: Date.now()
+                })
+                // after addind the todo item, clear the text
+                this.newTodoText = ''
+            }
+        },
+        removeTodo (item) {
+            // the function gets a todo item as an argument
+            // this.todos.filter() filters out all occurences of this item from the list this.todos
+            // we simply re-assign the filtered list of todos to list.todos
+            this.todos = this.todos.filter(_item => _item !== item)
+        }
     }
-  },
-};
+}
 </script>
 
 <style>
-html,
-body {
-  font: 16px/1.2 BlinkMacSystemFont, -apple-system, "Segoe UI", Roboto,
-    Helvetica, Arial, sans-serif;
+*, *::before, *::after {
+  box-sizing: border-box;
+}
+
+html, body {
+  font: 16px/1.2 BlinkMacSystemFont, -apple-system, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
   padding: 10px;
 }
 
@@ -86,76 +65,12 @@ body {
   margin: 0 auto;
 }
 
-.newtodo {
-  display: flex;
+#newtodo {
   width: 100%;
 }
 
-.todo {
-  display: flex;
-  width: 100%;
-  margin-bottom: 5px;
-}
-
-.list-header {
-  text-align: center;
-  padding: 5px;
-  margin-left: 2px;
-  margin-right: 2px;
-  background-color: #eee;
-  color: black;
-}
-
-.todo-text {
-  flex: 4;
-  text-align: center;
-}
-
-.todo-date {
-  flex: 3;
-  text-align: center;
-}
-
-.todo-add-button {
-  flex: 1;
-  border: 1px solid green;
-  background: green;
-  color: white;
-  font-size: 0.8rem;
-  padding: 2px 4px;
-  cursor: pointer;
-  margin-left: 2px;
-  margin-right: 2px;
-  outline: none;
-}
-
-.todo-remove-button {
-  flex: 1;
-  border: 1px solid red;
-  background-color: red;
-  color: white;
-  font-size: 0.8rem;
-  padding: 2px 4px;
-  cursor: pointer;
-  margin-left: 2px;
-  margin-right: 2px;
-}
-
-.todo-empty-button {
-  flex: 1;
-  padding: 2px 4px;
-  margin-left: 2px;
-  margin-right: 2px;
-}
-
-ul, li {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-}
-
-ul {
-    margin-top: 40px;
+input {
+  width: 90%;
 }
 
 button {
@@ -165,10 +80,42 @@ button {
   font-size: 0.8rem;
   padding: 2px 4px;
   cursor: pointer;
+  width: 10%;
+}
+
+
+form #todo-text {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #777;
+}
+
+ul, li {
+  margin: 0;
+  padding: 0;
 }
 
 p.none {
   color: #888;
   font-size: small;
+}
+
+.todo li {
+  display: flex;
+  margin: 5px 0;
+}
+
+.todo li span {
+  flex: 1;
+}
+
+
+.todo li button {
+  border: 1px solid red;
+  background: red;
+  color: white;
+  font-size: 0.8rem;
+  padding: 2px 4px;
+  cursor: pointer;
 }
 </style>
